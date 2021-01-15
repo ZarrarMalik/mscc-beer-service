9e51023ed8d6e7d8674801d422aae5dd38365298
Zarrar Malik <zarrarmalik@coop.co.uk>
2021-01-15 17:36:34 +0000

    starting a new project

---
+++ src/test/java/com/zarrar/mscc/beer/service/msccbeerservice/MsccBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zarrar.mscc.beer.service.msccbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsccBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
